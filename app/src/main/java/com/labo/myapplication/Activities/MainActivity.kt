bf1530f397986629a68d528cfe2f03ca2c3e1232
Alex7498-SV <00068817@uca.edu.sv>
2019-04-05 14:14:43 -0600

    commit #2

--- app/src/main/java/com/labo/myapplication/Activities/MainActivity.kt
+++ app/src/main/java/com/labo/myapplication/Activities/MainActivity.kt
@@ -3,8 +3,10 @@ package com.labo.myapplication.Activities
 import android.graphics.Movie
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import com.labo.myapplication.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,5 +18,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        initRecyclerView()
+
+
+
+
+    }
+
+    fun initRecyclerView(){
+        viewManager = LinearLayoutManager(this)
+
+        movieAdapter = MovieAdapter(movieList, {movieItem: Movie -> movieItemClicked(movieItem)})
+
+        movie_list_rv.apply{
+            setHasFixedSize(true)
+            layoutManager = viewManager
+            adapter = movieAdapter
+        }
     }
 }
